452fc787837cd7ecd45f2080a2cb07b07a92b18b
kemalulker <kemal.lkr@gmail.com>
2019-01-14 18:48:48 +0300

    add global db connection script for further usages

---
+++ dbconnection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$db = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+} 
+
+
+?>
