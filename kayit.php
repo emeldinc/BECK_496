72a207f07eaea8525aff796da2dc76f9773dc792
kemalulker <kemal.lkr@gmail.com>
2019-01-20 22:56:34 +0300

    Add redirecting to kayit

--- kayit.php
+++ kayit.php
@@ -1,12 +1,13 @@
 <?php
 include('dbconnection.php');
+
 $value = $_POST;
 $sql = "INSERT INTO beckdoor.user (username, firstname, lastname, role)
 VALUES ('".$value['username']."', '".$value['firstname']."', '".$value['lastname']."', '".$value['role']."')";
 
 if ($db->query($sql) === TRUE) {
-    echo "New record created successfully";
-
+    header('location: login.php');
+    $db->close();
 } else {
     echo "Error: " . $sql . "<br>" . $db->error;
 }
