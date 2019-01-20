d41ab19cadaadf44e14d2fb71db5d9656e751ceb
kemalulker <kemal.lkr@gmail.com>
2019-01-20 22:54:03 +0300

    Add utf8 format to dbconnection

--- dbconnection.php
+++ dbconnection.php
@@ -3,13 +3,15 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
+
 // Create connection
 $db = new mysqli($servername, $username, $password);
+mysqli_set_charset($db,'utf8');
 
 // Check connection
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
-}
+} 
 
 
 ?>
