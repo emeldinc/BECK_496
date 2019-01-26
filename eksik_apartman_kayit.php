c821143f6a243d22658e698011b643904f2c6621
kemalulker <kemal.lkr@gmail.com>
2019-01-26 14:45:20 +0300

    Edit not_existed_form backend

--- eksik_apartman_kayit.php
+++ eksik_apartman_kayit.php
@@ -6,7 +6,8 @@ $sql = "INSERT INTO beckdoor.apartman (ref_site_id, name, `number`)
 VALUES ('".$value['site_id']."', '".$value['isim']."', '".$value['numara']."')";
 
 if ($db->query($sql) === TRUE) {
-    header('location: eksik_daire_bilgileri.php');
+	$insert_id = $db->insert_id;
+    header("location: eksik_daire_bilgileri.php?insert_id=$insert_id");
     $db->close();
 } else {
     echo "Error: " . $sql . "<br>" . $db->error;
