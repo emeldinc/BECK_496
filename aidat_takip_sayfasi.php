53af756d8bd4b8ec677f6c86f668496e9d941a35
kemalulker <kemal.lkr@gmail.com>
2019-01-27 15:09:48 +0300

    Add aidat takip sayfasi basic html, edit duyurular html

--- aidat_takip_sayfasi.php
+++ aidat_takip_sayfasi.php
@@ -423,6 +423,7 @@
 <!-- END HEADER -->
 <div class="clearfix">
 </div>
+
 <!-- BEGIN CONTAINER -->
 <div class="page-container">
 	<!-- BEGIN SIDEBAR -->
@@ -495,6 +496,7 @@
             <!-- END SIDEBAR MENU -->
         </div>
     </div>
+
 	<!-- END SIDEBAR -->
 	<!-- BEGIN CONTENT -->
 	<div class="page-content-wrapper">
@@ -526,98 +528,10 @@
 			<div class="page-head">
 				<!-- BEGIN PAGE TITLE -->
 				<div class="page-title">
-					<h1>Duyurular</h1>
+					<h1>Aidat Takip Sistemi</h1>
 				</div>
 				<!-- END PAGE TITLE -->
-				<!-- BEGIN PAGE TOOLBAR -->
-				<div class="page-toolbar">
-					<!-- BEGIN THEME PANEL -->
-					<div class="btn-group btn-theme-panel">
-						<a href="javascript:;" class="btn dropdown-toggle" data-toggle="dropdown">
-						<i class="icon-settings"></i>
-						</a>
-						<div class="dropdown-menu theme-panel pull-right dropdown-custom hold-on-click">
-							<div class="row">
-								<div class="col-md-4 col-sm-4 col-xs-12">
-									<h3>THEME</h3>
-									<ul class="theme-colors">
-										<li class="theme-color theme-color-default active" data-theme="default">
-											<span class="theme-color-view"></span>
-											<span class="theme-color-name">Dark Header</span>
-										</li>
-										<li class="theme-color theme-color-light" data-theme="light">
-											<span class="theme-color-view"></span>
-											<span class="theme-color-name">Light Header</span>
-										</li>
-									</ul>
-								</div>
-								<div class="col-md-8 col-sm-8 col-xs-12 seperator">
-									<h3>LAYOUT</h3>
-									<ul class="theme-settings">
-										<li>
-											 Theme Style
-											<select class="layout-style-option form-control input-small input-sm">
-												<option value="square" selected="selected">Square corners</option>
-												<option value="rounded">Rounded corners</option>
-											</select>
-										</li>
-										<li>
-											 Layout
-											<select class="layout-option form-control input-small input-sm">
-												<option value="fluid" selected="selected">Fluid</option>
-												<option value="boxed">Boxed</option>
-											</select>
-										</li>
-										<li>
-											 Header
-											<select class="page-header-option form-control input-small input-sm">
-												<option value="fixed" selected="selected">Fixed</option>
-												<option value="default">Default</option>
-											</select>
-										</li>
-										<li>
-											 Top Dropdowns
-											<select class="page-header-top-dropdown-style-option form-control input-small input-sm">
-												<option value="light">Light</option>
-												<option value="dark" selected="selected">Dark</option>
-											</select>
-										</li>
-										<li>
-											 Sidebar Mode
-											<select class="sidebar-option form-control input-small input-sm">
-												<option value="fixed">Fixed</option>
-												<option value="default" selected="selected">Default</option>
-											</select>
-										</li>
-										<li>
-											 Sidebar Menu
-											<select class="sidebar-menu-option form-control input-small input-sm">
-												<option value="accordion" selected="selected">Accordion</option>
-												<option value="hover">Hover</option>
-											</select>
-										</li>
-										<li>
-											 Sidebar Position
-											<select class="sidebar-pos-option form-control input-small input-sm">
-												<option value="left" selected="selected">Left</option>
-												<option value="right">Right</option>
-											</select>
-										</li>
-										<li>
-											 Footer
-											<select class="page-footer-option form-control input-small input-sm">
-												<option value="fixed">Fixed</option>
-												<option value="default" selected="selected">Default</option>
-											</select>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- END THEME PANEL -->
-				</div>
-				<!-- END PAGE TOOLBAR -->
+			
 			</div>
 			<!-- END PAGE HEAD -->
 			<!-- BEGIN PAGE BREADCRUMB -->
@@ -627,88 +541,31 @@
 					<i class="fa fa-circle"></i>
 				</li>
 				<li>
-					<a href="#">Duyurular</a>
+					<a href="#">Aidat Takip Sistemi</a>
 				</li>
 			</ul>
+			<div class="page-actions">
+			<div class="btn-group">
+				<button type="button" class="btn red-haze btn-sm dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+				<span class="hidden-sm hidden-xs">Seçenekler&nbsp;</span><i class="fa fa-angle-down"></i>
+				</button>
+				<ul class="dropdown-menu" role="menu">
+					<li>
+						<a href="yeni_aidat.php">
+						<i class="icon-docs"></i> Yeni Aidat Ekle </a>
+					</li>
+					<li>
+						<a href="javascript:;">
+						<i class="icon-share"></i> Paylaş </a>
+					</li>
+				</ul>
+			</div>
+			</div>
+			<br />
 			<!-- END PAGE BREADCRUMB -->
 			<!-- END PAGE HEADER-->
 			<!-- BEGIN PAGE CONTENT-->
-
-			<div class="page-actions">
-				<div class="btn-group">
-					<button type="button" class="btn red-haze btn-sm dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-					<span class="hidden-sm hidden-xs">Seçenekler&nbsp;</span><i class="fa fa-angle-down"></i>
-					</button>
-					<ul class="dropdown-menu" role="menu">
-						<li>
-							<a href="duyuruekle.php">
-							<i class="icon-docs"></i> Yeni Duyuru Ekle </a>
-						</li>
-						<li>
-							<a href="javascript:;">
-							<i class="icon-share"></i> Paylaş </a>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<br />
-			<div class="portlet light">
-				<div class="portlet-body">
-					<div class="row">
-						<div class="col-md-12">
-							<ul class="timeline">
-								<li class="timeline-yellow">
-									<div class="timeline-time">
-										<span class="date">
-										22/01/19 </span>
-										<span class="time">
-										18:30 </span>
-									</div>
-									<div class="timeline-icon">
-										<i class="icon-pin"></i>
-									</div>
-									<div class="timeline-body">
-										<h2>Etkinlik Duyurusu</h2>
-										<div class="timeline-content">
-											<img class="timeline-img pull-left" src="../../assets/admin/pages/media/blog/2.jpg" alt="">
-											23 Şubat 19.00'da Site sakinlerine akşam yemeği düzenlenecektir.
-										</div>
-										<div class="timeline-footer">
-											<a href="javascript:;" class="nav-link pull-right">
-											Read more <i class="m-icon-swapright m-icon-white"></i>
-											</a>
-										</div>
-									</div>
-								</li>
-								<li class="timeline-red">
-									<div class="timeline-time">
-										<span class="date">
-										24/01/19 </span>
-										<span class="time">
-										12:04 </span>
-									</div>
-									<div class="timeline-icon">
-										<i class="icon-home"></i>
-									</div>
-									<div class="timeline-body">
-										<h2>Toplantı Duyurusu</h2>
-										<div class="timeline-content">
-											<img class="timeline-img pull-right" src="../../assets/admin/pages/media/blog/1.jpg" alt="">
-											 12 Şubat 19.00'da site yönetim toplantısı yapılacaktır.
-										</div>
-										<div class="timeline-footer">
-											<a href="javascript:;" class="nav-link">
-											Read more <i class="m-icon-swapright m-icon-white"></i>
-											</a>
-										</div>
-									</div>
-
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
+			
 			<!-- END PAGE CONTENT-->
 		</div>
 	</div>
