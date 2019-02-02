e401a90c6931827b175bc7c8b9bdbd8f9eb7d9ae
kemalulker <kemal.lkr@gmail.com>
2019-02-02 19:26:20 +0300

    Add table which lists incomes-outcomes with edit and delete option, also user role controlled

---
+++ gelir_gider_duzenle.php
@@ -0,0 +1,28 @@
+<?php 
+
+	if(session_id() == '')
+    session_start();
+	include('dbconnection.php');
+
+    $miktar = $_POST['miktar'];
+    $tarih = $_POST['tarih'];
+    $gelir_gider_id = $_GET['gelir_gider_id'];
+    $aciklama = $_POST['description'];
+
+    $sql = "UPDATE beckdoor.gelir_gider SET amount = '".$miktar."', date = '".$tarih."',description = '".$aciklama."' 
+    WHERE id = '".$gelir_gider_id."'";
+
+    if(mysqli_query($db,$sql)) {
+    	header('location: aidat_takip_sayfasi.php');
+    }
+
+    else {
+        echo $db->error;
+        echo "<script> alert('Kaydiniz olusturulamamistir...') </script>";
+    }
+
+   
+
+
+
+    ?>
