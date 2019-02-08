26e103b7ea59ea99ea0473cd9ca20e1b73a6fe12
kemalulker <kemal.lkr@gmail.com>
2019-02-08 13:34:54 +0300

    Add link to logo and sidebar-header made fixed

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
          <div class="page-header-inner">
             <!-- BEGIN LOGO -->
             <div class="page-logo">
-               <a href="index.html">
+               <a href="index.php">
                <img src="assets/admin/layout4/img/becklogo.png" alt="logo" class="logo-default"/>
                </a>
                <div class="menu-toggler sidebar-toggler">
