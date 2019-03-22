2ab427b549e7ee63336f71269307f34bb51e0ba5
SengulolanCem <c.sengul@etu.edu.tr>
2019-03-22 13:59:37 +0300

    Fixed API duyurular

--- Android_API/duyurular.php
+++ Android_API/duyurular.php
@@ -1,24 +1,32 @@
 <?php
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: access");
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Allow-Credentials: true");
+    header('Content-Type: application/json');
+
     include '../dbconnection.php';
-    if(isset($_POST['duyuru']))
+
+    $response = array("error" => FALSE,"duyurular" => array());
+    $apartman_id = $_GET['duyuru'];
+    $sql = "SELECT * FROM `duyuru` WHERE `ref_apartman_id` = $apartman_id";
+    $res = mysqli_query($db,$sql);
+    while($row = mysqli_fetch_assoc($res))
     {
-        $response = array("error" => FALSE,"duyurular" => array());
-        $apartman_id = $_POST['duyuru'];
-        $sql = "SELECT * FROM `duyuru` WHERE `ref_apartman_id` = $apartman_id";
-        $res = mysqli_query($db,$sql);
-        while($row = mysqli_fetch_assoc($res))
-        {
-            $duyuru = array();
-            $namesql = "SELECT * FROM `user` WHERE `id` = ".$row['ref_user_id'];
-            $innerres = mysqli_query($db,$innerres);
-            $innerrow = mysqli_fetch_assoc($innerres);
-            $duyuru['fullname'] = $innerrow['firstname']." ".$innerrow['lastname'];
-            $duyuru['title'] = $row['title'];
-            $duyuru['description'] = $row['description'];
-            $duyuru['date'] = $row['now_date'];
-            array_push($response['duyurular'],$duyuru);
-        }
-        echo json_encode($response);
+        $duyuru = array();
+        $sql = "SELECT * FROM `user` WHERE `id` = ".$row['ref_user_id'];
+
+        $innerres = mysqli_query($db,$sql);
+        $innerrow = mysqli_fetch_assoc($innerres);
+
+        $duyuru['fullname'] = $innerrow['firstname']." ".$innerrow['lastname'];
+        $duyuru['title'] = $row['title'];
+        $duyuru['description'] = $row['description'];
+        $duyuru['date'] = $row['now_date'];
+        array_push($response['duyurular'],$duyuru);
     }
+    echo json_encode($response);
+
 
 ?>
