4480ec65e3fef66896772d6d7559b104c5663789
SengulolanCem <c.sengul@etu.edu.tr>
2019-03-22 22:05:15 +0300

    Added API base for financial info

---
+++ Android_API/finance.php
@@ -0,0 +1,41 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-type: application/json; charset=utf-8');
+
+include '../dbconnection.php';
+
+$response = array("error" => FALSE,"finances" => array());
+$apartman_id = $_GET['finance'];
+
+$sql = "SELECT * FROM `aidat` WHERE `ref_apartman_id` = $apartman_id";
+$res = mysqli_query($db,$sql);
+while($row = mysqli_fetch_assoc($res))
+{
+    $finance = array();
+    $finance['type'] = "aidat";
+    $finance['daire_no'] = $row['ref_daire_id'];
+    $finance['amount'] = $row['amount'];
+    $finance['date'] = $row['date'];
+    $finance['odendiMi'] = $row['odendiMi'];
+    array_push($response['finances'],$finance);
+}
+
+$sql = "SELECT * FROM `gelir_gider` WHERE `ref_apartman_id` = $apartman_id";
+$res = mysqli_query($db,$sql);
+while($row = mysqli_fetch_assoc($res))
+{
+    $finance = array();
+    $finance['type'] = "gelir_gider";
+    $finance['amount'] = $row['amount'];
+    $finance['date'] = $row['date'];
+    $finance['gelirMi'] = $row['odendiMi'];
+    array_push($response['finances'],$finance);
+}
+echo json_encode($response);
+
+
+?>
