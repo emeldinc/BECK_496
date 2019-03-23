65211c73bd357c13686ec5a096a70db7cc067683
SengulolanCem <c.sengul@etu.edu.tr>
2019-03-23 12:29:36 +0300

    API for Announcements changed

--- Android_API/duyuru_ekle.php
+++ Android_API/duyuru_ekle.php
@@ -2,18 +2,18 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Credentials: true");
 header('Content-type: application/x-wwww-form-urlencoded; charset=utf-8');
 
 include '../dbconnection.php';
 include ('Utility.php');
 
-$user_id = $_POST['user_id'];
-$apartman_id = getApartmentID($_POST['house_id']);
+$user_id = $_GET['user_id'];
+$apartman_id = getApartmentID($_GET['house_id']);
 $site_id = getSiteID($apartman_id);
-$title = $_POST['title'];
-$description = $_POST['description'];
+$title = $_GET['title'];
+$description = $_GET['description'];
 
 $response = array("error" => TRUE,"duyurular" => array());
 
