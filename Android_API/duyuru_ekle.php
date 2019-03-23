5537064956325bebcff25877062c747925b998b1
SengulolanCem <c.sengul@etu.edu.tr>
2019-03-23 05:53:02 +0300

    changed to post

--- Android_API/duyuru_ekle.php
+++ Android_API/duyuru_ekle.php
@@ -2,7 +2,7 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Credentials: true");
 header('Content-type: application/json; charset=utf-8');
 
